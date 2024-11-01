5d885e0317a12616c54c839a1097f0fe5961896b
mslmyrtd <mslmyrtd@gmail.com>
2024-11-02 02:02:10 +0300

    connectionstring defined and maked first migrate

--- GameStore/obj/Debug/net8.0/GameStore.AssemblyInfo.cs
+++ GameStore/obj/Debug/net8.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbc066cfa11b5db41206799096bde0ecdc3facb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8d3737ea19082109c32e6c0a579b8769bdb619d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
